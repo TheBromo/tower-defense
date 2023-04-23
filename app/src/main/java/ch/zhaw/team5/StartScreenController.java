3fcc869b596b9dd2b9d036801779dc2d474a30f3
kuengpas <pascal-kueng@gmx.net>
2023-04-23 17:38:53 +0200

    implemented Controller Class

---
+++ app/src/main/java/ch/zhaw/team5/StartScreenController.java
@@ -0,0 +1,14 @@
+package ch.zhaw.team5;
+
+import javafx.event.ActionEvent;
+
+public class StartScreenController {
+    public void onStartButtonClicked (ActionEvent event) {
+        System.out.println("Start Button clicked");
+        // TODO: Hier kommt dann Logik von Micha
+    }
+
+    public void onQuitButtonClicked (ActionEvent event) {
+        System.exit(0);
+    }
+}
