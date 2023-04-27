edb6ebee39304592dedbb7a374d9e380b41204c6
michamettler <mettlermicha@gmail.com>
2023-04-28 00:32:38 +0200

    UI changes and added GameViewController base

---
+++ app/src/main/java/ch/zhaw/team5/GameViewController.java
@@ -0,0 +1,15 @@
+package ch.zhaw.team5;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GameViewController {
+
+}
--- app/src/main/java/ch/zhaw/team5/StartScreenController.java
+++ app/src/main/java/ch/zhaw/team5/StartScreenController.java
@@ -2,7 +2,13 @@ package ch.zhaw.team5;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StartScreenController {
     @FXML
@@ -10,8 +16,15 @@ public class StartScreenController {
     @FXML
     public Button quitButton;
 
-    public void onStartButtonClicked(ActionEvent event) {
-        // TODO: Hier kommt dann Logik von Micha
+    public void onStartButtonClicked(ActionEvent event) throws IOException {
+        FXMLLoader resultWindowLoader = FXMLLoader.load(getClass().getResource("GameWindow.fxml"));
+        Parent root1 = (Parent) resultWindowLoader.load();
+
+        GameViewController controller = resultWindowLoader.getController();
+        Stage stage = new Stage();
+        stage.setTitle("Result Window");
+        stage.setScene(new Scene(root1));
+        stage.show();
     }
 
     public void onQuitButtonClicked(ActionEvent event) {
