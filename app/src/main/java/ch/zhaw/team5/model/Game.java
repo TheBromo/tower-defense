205121607d670aec6d225e2af2a6b4d9c7dc7251
kuengpas <pascal-kueng@gmx.net>
2023-04-28 10:10:20 +0200

    - added arrows

--- app/src/main/java/ch/zhaw/team5/model/Game.java
+++ app/src/main/java/ch/zhaw/team5/model/Game.java
@@ -1,6 +1,8 @@
 package ch.zhaw.team5.model;
 
 public class Game implements Renderable{
+
+    //TODO: Sind dies alle Objekte?
     private Tower tower;
     private Wall wall;
     private Phase phase;
@@ -8,8 +10,9 @@ public class Game implements Renderable{
     private Enemy enemy;
     private Money money;
     private Player player;
+    private Arrow arrow;
 
-    public Game(Tower tower, Wall wall, Phase phase, Path path, Enemy enemy, Money money, Player player) {
+    public Game(Tower tower, Wall wall, Phase phase, Path path, Enemy enemy, Money money, Player player, Arrow arrow) {
         this.tower = tower;
         this.wall = wall;
         this.phase = phase;
@@ -17,8 +20,10 @@ public class Game implements Renderable{
         this.enemy = enemy;
         this.money = money;
         this.player = player;
+        this.arrow = arrow;
     }
 
+    //TODO: Wie wird render implementiert?
     @Override
     public void render() {
 
