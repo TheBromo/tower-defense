c9ab1610f2f65ed092f5204fb7fb96afa4b2c646
hartmdo1 <hartmdo1@zhaw.students.ch>
2023-05-12 16:14:25 +0200

    - added JavaDoc to the Phases Classes

--- app/src/main/java/ch/zhaw/team5/model/phases/PausePhase.java
+++ app/src/main/java/ch/zhaw/team5/model/phases/PausePhase.java
@@ -1,25 +1,45 @@
 package ch.zhaw.team5.model.phases;
 
 import java.util.concurrent.TimeUnit;
-
+/**
+ * PausePhase Class extends Phase. 
+ *
+ * @author strenman
+ * @version 1.0.0
+ */
 public class PausePhase extends Phase {
     private int count = 0;
 
+    /**
+     * Constructor of PausePhase
+     * @param totalTimeSeconds represents the time of pause
+     */
     public PausePhase( long totalTimeSeconds) {
         super( totalTimeSeconds);
     }
 
+    /**
+     * Increase difficulty
+     */
     @Override
     public void increaseDifficulty() {
         totalTimeMS += TimeUnit.SECONDS.toMillis(10);
         count++;
     }
 
+    /**
+     * Get the amount of the enemy
+     * @return 0
+     */
     @Override
     public int getEnemyAmount() {
         return 0;
     }
 
+    /**
+     * Override toString() method
+     * @return a string with the amount
+     */
     @Override
     public String toString() {
         return "Pause " + count;
--- app/src/main/java/ch/zhaw/team5/model/phases/Phase.java
+++ app/src/main/java/ch/zhaw/team5/model/phases/Phase.java
@@ -1,13 +1,22 @@
 package ch.zhaw.team5.model.phases;
 
 import java.util.concurrent.TimeUnit;
-
+/**
+ * This class represents the abstract Phase class.
+ *
+ * @author strenman
+ * @version 1.0.0
+ */
 public abstract class Phase {
     protected long intveralMS;
     protected long phaseStart;
     protected long lastUpdate;
     protected long totalTimeMS;
 
+    /**
+     * Constructor of Phase class.
+     * @param totalTimeSeconds represents the amount of time the phase last. 
+     */
     public Phase(long totalTimeSeconds) {
         intveralMS = 50;
         totalTimeMS = TimeUnit.SECONDS.toMillis(totalTimeSeconds);
@@ -15,28 +24,53 @@ public abstract class Phase {
         lastUpdate = System.currentTimeMillis();
     }
 
+    /**
+     * Method to get the progress of the phase.
+     * @return the time in a double value of the current phase progress
+     */
     public double getPhaseProgress() {
         return (100.0 / totalTimeMS) * (lastUpdate - phaseStart);
     }
 
+    /**
+     * Checks if the phase has ended
+     * @return boolean yes / no
+     */
     public boolean hasEnded() {
         return lastUpdate - phaseStart > totalTimeMS;
     }
 
+    /**
+     * Updates the phase
+     */
     public void updatePhase() {
         if (System.currentTimeMillis() - lastUpdate >= intveralMS && !hasEnded()) {
             lastUpdate = System.currentTimeMillis();
         }
     }
 
+    /**
+     * Restart the timer
+     */
     public void restartTimer() {
         phaseStart = System.currentTimeMillis();
         lastUpdate = System.currentTimeMillis();
     }
 
+    /**
+     * abstract method to increase the difficulty
+     */
     public abstract void increaseDifficulty();
 
+    /**
+     * abstract method to get the amount of the enemies
+     * @return
+     */
     public abstract int getEnemyAmount();
 
+    /**
+     * abstract method to implement the toString() method
+     * @return
+     */
     public abstract String toString();
 }
