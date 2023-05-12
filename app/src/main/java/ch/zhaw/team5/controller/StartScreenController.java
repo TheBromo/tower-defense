dcef4822a5a9e0174afb94849dbd54a8764644cf
kuengpas <pascal-kueng@gmx.net>
2023-05-12 15:51:52 +0200

    - JavaDoc for StartScreenController class

--- app/src/main/java/ch/zhaw/team5/controller/StartScreenController.java
+++ app/src/main/java/ch/zhaw/team5/controller/StartScreenController.java
@@ -10,12 +10,24 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+/**
+ * This class acts as a controller for the start screen in the Tower Defense game.
+ * @author kuengpas
+ * @version 1.0.0
+ */
 public class StartScreenController {
     @FXML
     public Button startButton;
     @FXML
     public Button quitButton;
 
+    /**
+     * Handles the click event of the start button. This method will load the game
+     * view, create a new stage for it, and then close the current stage.
+     *
+     * @param event the ActionEvent to be handled
+     * @throws IOException if there's an error loading the FXML file
+     */
     public void onStartButtonClicked(ActionEvent event) throws IOException {
 
         FXMLLoader gameViewLoader = new FXMLLoader(getClass().getResource("GameView.fxml"));
@@ -34,6 +46,12 @@ public class StartScreenController {
         currentStage.close();
     }
 
+    /**
+     * Handles the click event of the quit button. This method will exit the
+     * application.
+     *
+     * @param event the ActionEvent to be handled
+     */
     public void onQuitButtonClicked(ActionEvent event) {
         System.exit(0);
     }
