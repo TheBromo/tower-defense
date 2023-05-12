ecd5bc0eba8e93f6d68cf6a8c1c6f9069acc9d77
kuengpas <pascal-kueng@gmx.net>
2023-05-12 16:47:47 +0200

    - Javadoc for Tower class

--- app/src/main/java/ch/zhaw/team5/model/gameobj/Tower.java
+++ app/src/main/java/ch/zhaw/team5/model/gameobj/Tower.java
@@ -9,16 +9,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * The Tower class represents a tower in the game that can shoot at enemies.
+ * It extends the StaticGameObject class and contains methods for upgrading the tower and shooting at enemies.
+ * @author strenman mettlmi1
+ * @version 1.0.0
+ */
 public class Tower extends StaticGameObject {
 
     public final int range = 200;
     public final int price = 50;
 
-    private int damge = 20;
+    private int damage = 20;
     private long interval = 4000, lastShot = 0;
 
     private List<Arrow> arrows;
 
+    /**
+     * Creates a new Tower object at the specified position.
+     *
+     * @param position the position of the tower
+     */
     public Tower(Point2D position) {
         super(position, Sprite.SpritePath.SMALLTOWER);
         arrows = new ArrayList<>();
@@ -26,6 +37,11 @@ public class Tower extends StaticGameObject {
         width = 100;
     }
 
+    /**
+     * Renders the tower and its arrows on the provided canvas.
+     *
+     * @param canvas the canvas to draw the tower and arrows on
+     */
     @Override
     public void render(Canvas canvas) {
         canvas.getGraphicsContext2D().drawImage(sprite.getSprite(), position.getX() - width / 2,
@@ -37,9 +53,12 @@ public class Tower extends StaticGameObject {
         }
 
     }
-
+    
+    /**
+     * Upgrades the tower, increasing its damage and decreasing the interval between shots.
+     */
     public void upgrade() {
-        damge += 8;
+        damage += 8;
         interval *= .75;
     }
 
@@ -47,6 +66,11 @@ public class Tower extends StaticGameObject {
         return position.distance(enemy.getPosition()) > range;
     }
 
+    /**
+     * Shoots at the provided list of enemies if the interval between shots has passed.
+     *
+     * @param enemies the list of enemies to shoot at
+     */
     public void shootAtEnemies(List<Enemy> enemies) {
 
         if (enemies.size() > 0 && interval < System.currentTimeMillis() - lastShot) {
@@ -60,11 +84,14 @@ public class Tower extends StaticGameObject {
             Random rand = new Random();
             var target = possibleTargets.get(rand.nextInt(possibleTargets.size()));
 
-            arrows.add(new Arrow(target, this.position, damge));
+            arrows.add(new Arrow(target, this.position, damage));
             lastShot = System.currentTimeMillis();
         }
     }
 
+    /**
+     * Updates the tower, removing any arrows that have hit their target and updating the remaining arrows.
+     */
     public void update() {
         arrows.removeIf(Arrow::hasHitTarget);
         for (Arrow arrow : arrows) {
