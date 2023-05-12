ac7049ebb5df1f7c770f96838358a1f70f75bb26
kuengpas <pascal-kueng@gmx.net>
2023-05-12 15:57:34 +0200

    - JavaDoc for ImageLoader class

--- app/src/main/java/ch/zhaw/team5/model/util/ImageLoader.java
+++ app/src/main/java/ch/zhaw/team5/model/util/ImageLoader.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 
 import javafx.scene.image.Image;
 
+/**
+ * Singleton class to manage image loading in the Tower Defense game.
+ * @author strenman
+ * @version 1.0.0
+ */
 public class ImageLoader {
 
     private static ImageLoader instance = new ImageLoader();
@@ -11,6 +16,11 @@ public class ImageLoader {
     private ImageLoader() {
     }
 
+    /**
+     * Provides global access to the singleton instance of ImageLoader.
+     *
+     * @return the instance of ImageLoader
+     */
     public static ImageLoader getInstance() {
         if (instance == null) {
             instance = new ImageLoader();
@@ -18,6 +28,12 @@ public class ImageLoader {
         return instance;
     }
 
+    /**
+     * Returns an Image by its name.
+     *
+     * @param name the name of the image
+     * @return the Image, or null if there's an error loading the image
+     */
     public Image getByName(String name) {
         var url = ClassLoader.getSystemClassLoader().getResource("images/" + name + ".PNG");
         try {
